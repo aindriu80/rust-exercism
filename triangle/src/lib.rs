5508134efd49817bd296ed380664032d4906f9ae
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2025-04-13 15:51:53 +0100

    Passing the triangle tests

--- triangle/src/lib.rs
+++ triangle/src/lib.rs
@@ -1,7 +1,6 @@
 pub struct Triangle {
     sides: [u64; 3],
 }
-enum TriangleType {}
 
 impl Triangle {
     pub fn build(sides: [u64; 3]) -> Option<Triangle> {
@@ -9,22 +8,33 @@ impl Triangle {
             return None;
         }
 
-        if sides[0] == sides[1] && sides[1] == sides[2] && sides[0] == sides[2] {
-            Some(Triangle {}) // Return Some with a Triangle instance
-        } else {
-            None
+        // Check triangle inequality
+        if sides[0] + sides[1] <= sides[2]
+            || sides[1] + sides[2] <= sides[0]
+            || sides[0] + sides[2] <= sides[1]
+        {
+            return None;
         }
+
+        // Create a new Triangle with the given sides
+        Some(Triangle { sides })
     }
 
     pub fn is_equilateral(&self) -> bool {
-        todo!("this");
+        self.sides[0] == self.sides[1] && self.sides[1] == self.sides[2]
     }
 
     pub fn is_scalene(&self) -> bool {
-        todo!("Determine if the Triangle is scalene.");
+        // A scalene triangle has NO equal sides
+        self.sides[0] != self.sides[1]
+            && self.sides[1] != self.sides[2]
+            && self.sides[0] != self.sides[2]
     }
 
     pub fn is_isosceles(&self) -> bool {
-        todo!("Determine if the Triangle is isosceles.");
+        // An isosceles triangle has AT LEAST two equal sides
+        self.sides[0] == self.sides[1]
+            || self.sides[1] == self.sides[2]
+            || self.sides[0] == self.sides[2]
     }
 }
