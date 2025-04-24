c59a252779a1d514aefd619efe15273ef4fcd227
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2025-04-24 19:52:26 +0100

    Custom Sets - Adding the functionality, completing the tests

--- custom-set/src/lib.rs
+++ custom-set/src/lib.rs
@@ -1,53 +1,94 @@
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug)]
 pub struct CustomSet<T> {
-    // We fake using T here, so the compiler does not complain that
-    // "parameter `T` is never used". Delete when no longer needed.
-    // phantom: std::marker::PhantomData<T>,
     data: Vec<T>,
 }
+impl<T: PartialEq> PartialEq for CustomSet<T> {
+    fn eq(&self, other: &Self) -> bool {
+        if self.data.len() != other.data.len() {
+            return false;
+        }
+        self.data.iter().all(|elem| other.data.contains(elem))
+    }
+}
+
+impl<T: PartialEq> Eq for CustomSet<T> {}
 
 impl<T> CustomSet<T>
 where
-    T: Clone,
+    T: Clone + PartialEq,
 {
     pub fn new(input: &[T]) -> Self {
-        CustomSet {
-            data: input.to_vec(),
+        let mut data = Vec::new();
+        for item in input {
+            if !data.contains(item) {
+                data.push(item.clone());
+            }
+        }
+        CustomSet { data }
+    }
+
+    pub fn contains(&self, element: &T) -> bool {
+        self.data.contains(element)
+    }
+
+    pub fn add(&mut self, element: T) {
+        if !self.contains(&element) {
+            self.data.push(element);
         }
     }
 
-    pub fn contains(&self, _element: &T) -> bool {
-        todo!();
-    }
-
-    pub fn add(&mut self, _element: T) {
-        todo!();
-    }
-
-    pub fn is_subset(&self, _other: &Self) -> bool {
-        todo!();
+    pub fn is_subset(&self, other: &Self) -> bool {
+        if self.is_empty() {
+            return true;
+        } else if other.is_empty() {
+            return false;
+        } else {
+            self.data.iter().all(|elem| other.contains(elem))
+        }
     }
 
     pub fn is_empty(&self) -> bool {
         self.data.is_empty()
     }
 
-    pub fn is_disjoint(&self, _other: &Self) -> bool {
-        todo!();
+    pub fn is_disjoint(&self, other: &Self) -> bool {
+        if self.is_empty() || other.is_empty() {
+            return true;
+        } else {
+            self.data.iter().all(|elem| !other.contains(elem))
+        }
     }
 
     #[must_use]
-    pub fn intersection(&self, _other: &Self) -> Self {
-        todo!();
+    pub fn intersection(&self, other: &Self) -> Self {
+        let data = self
+            .data
+            .iter()
+            .filter(|elem| other.contains(elem))
+            .cloned()
+            .collect::<Vec<T>>();
+        CustomSet { data }
     }
 
     #[must_use]
-    pub fn difference(&self, _other: &Self) -> Self {
-        todo!();
+    pub fn difference(&self, other: &Self) -> Self {
+        let data = self
+            .data
+            .iter()
+            .filter(|elem| !other.contains(elem))
+            .cloned()
+            .collect::<Vec<T>>();
+        CustomSet { data }
     }
 
     #[must_use]
-    pub fn union(&self, _other: &Self) -> Self {
-        todo!();
+    pub fn union(&self, other: &Self) -> Self {
+        let mut data = self.data.clone();
+        for elem in &other.data {
+            if !data.contains(elem) {
+                data.push(elem.clone());
+            }
+        }
+        CustomSet { data }
     }
 }
