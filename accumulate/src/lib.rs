fa6c8d0d351c6a894378b5ecc9df0a2ab24360d9
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2025-04-26 19:53:35 +0100

    Accumulate - completing the tests

--- accumulate/src/lib.rs
+++ accumulate/src/lib.rs
@@ -1,4 +1,6 @@
-/// What should the type of _function be?
-pub fn map(input: Vec<i32>, _function: ???) -> Vec<i32> {
-    todo!("Transform input vector {input:?} using passed function");
+pub fn map<T, U, F>(input: Vec<T>, function: F) -> Vec<U>
+where
+    F: FnMut(T) -> U,
+{
+    input.into_iter().map(function).collect()
 }
