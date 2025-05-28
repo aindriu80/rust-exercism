c30a346efa9bc4201df64f718052defef5f629ad
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2025-05-28 15:41:31 +0100

    Completing the tests for secret handshake

--- secret-handshake/src/lib.rs
+++ secret-handshake/src/lib.rs
@@ -1,3 +1,15 @@
 pub fn actions(n: u8) -> Vec<&'static str> {
-    todo!("What is the secret handshake for {n}?")
+    let messages = ["wink", "double blink", "close your eyes", "jump"];
+    let mut result = Vec::new();
+
+    for i in 0..4 {
+        if n & (1 << i) != 0 {
+            result.push(messages[i]);
+        }
+    }
+
+    if n & (1 << 4) != 0 {
+        result.reverse();
+    }
+    result
 }
