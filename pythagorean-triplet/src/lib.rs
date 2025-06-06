2b5572fd2322c20ccf21b74190bfcb81db4a2056
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2025-06-06 14:58:32 +0100

    Tests to find all Pythagorean triplets that satisfy the condition

--- pythagorean-triplet/src/lib.rs
+++ pythagorean-triplet/src/lib.rs
@@ -1,7 +1,18 @@
 use std::collections::HashSet;
 
 pub fn find(sum: u32) -> HashSet<[u32; 3]> {
-    todo!(
-        "Given the sum {sum}, return all possible Pythagorean triplets, which produce the said sum, or an empty HashSet if there are no such triplets. Note that you are expected to return triplets in [a, b, c] order, where a < b < c"
-    );
+    let mut triplets: HashSet<[u32; 3]> = HashSet::new();
+
+    for a in 1..sum / 3 {
+        for b in (a + 1)..=(sum - a) / 2 {
+            let c = sum - a - b;
+
+            if a * a + b * b == c * c {
+                let triplet = [a, b, c];
+                triplets.insert(triplet);
+            }
+        }
+    }
+
+    triplets
 }
